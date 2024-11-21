044b9e062e168bf20b0ced035accd7132685678e
User <user@MB033361.local>
2024-11-21 11:30:30 -0300

    show the questions when pressing the buttons true or false

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: - IBOutlets
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    // MARK: - properties
+    let quiz = [
+        "4 + 2 = 6",
+        "(5 - 3) > 1",
+        "(3 + 8) < 10"
+    ]
+    var questionNumber = 0
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateUI()
     }
 
-
+    // MARK: - IBActions
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        questionNumber += 1
+        updateUI()
+    }
+    
+    // MARK: - functions
+    private func updateUI() {
+        questionLabel.text = quiz[questionNumber]
+    }
 }
 
